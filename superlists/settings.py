d54130bb6e184e578d09566b784d91a93cdebacb
Dima <random1k11@gmail.com>
2019-08-10 16:03:25 +0300

    Created projects app

--- superlists/settings.py
+++ superlists/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'projects',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'superlists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blog/tempalates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
